cb0323e82fefa6e3e630bb8abac8ef080166e6fe
Yari-Ros <2118088@talnet.nl>
2020-02-06 13:16:48 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,3 @@
+use sterrenstelsel;
+insert into planeten (name, diameter, distance_from_the_sun, mass)
+values ('Zon', '1.392.000 km', '-', '332.946'), ('Mercurius', '4.880', '57.910.000', '0,1'), ('Venus', '12.104', '108.208.930', '0.9'), ('Aarde', '12.756', '149.208.930', '1'), ('Mars', '6.794', '227.936.640', '0.1')
